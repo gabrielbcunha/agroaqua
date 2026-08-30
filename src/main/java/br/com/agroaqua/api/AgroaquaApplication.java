326832bafbf40cecee5b7a6cf50eca9bf9d27a27
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-08-30 10:38:51 -0300

    Project start

---
+++ src/main/java/br/com/agroaqua/api/AgroaquaApplication.java
@@ -0,0 +1,13 @@
+package br.com.agroaqua.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroaquaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroaquaApplication.class, args);
+    }
+
+}
